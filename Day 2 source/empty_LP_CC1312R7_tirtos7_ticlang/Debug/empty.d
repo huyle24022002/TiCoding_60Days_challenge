4952b221b77df86dc70bbf35d5eaa3e5fb4f3099
huyle24022002 <79469268+huyle24022002@users.noreply.github.com>
2026-02-07 15:11:14 +0800

    Add files via upload

---
+++ Day 2 source/empty_LP_CC1312R7_tirtos7_ticlang/Debug/empty.d
@@ -0,0 +1,69 @@
+# FIXED
+
+empty.o: ../empty.c \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/posix/ticlang/unistd.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/posix/ticlang/sys/types.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/posix/ticlang/sys/_internal.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/drivers/GPIO.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/DeviceFamily.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/drivers/gpio/GPIOCC26XX.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/inc/hw_ioc.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/driverlib/ioc.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/inc/hw_types.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/inc/hw_chip_def.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/inc/hw_memmap.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/inc/hw_ioc.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/inc/hw_ints.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/driverlib/interrupt.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/inc/hw_nvic.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/driverlib/debug.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/driverlib/cpu.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/inc/hw_cpu_scs.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/driverlib/rom.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/driverlib/gpio.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/inc/hw_gpio.h \
+ syscfg/ti_drivers_config.h
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/posix/ticlang/unistd.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/posix/ticlang/sys/types.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/posix/ticlang/sys/_internal.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/drivers/GPIO.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/DeviceFamily.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/drivers/gpio/GPIOCC26XX.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/inc/hw_ioc.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/driverlib/ioc.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/inc/hw_types.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/inc/hw_chip_def.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/inc/hw_memmap.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/inc/hw_ioc.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/inc/hw_ints.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/driverlib/interrupt.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/inc/hw_nvic.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/driverlib/debug.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/driverlib/cpu.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/inc/hw_cpu_scs.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/driverlib/rom.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/driverlib/gpio.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source/ti/devices/cc13x2x7_cc26x2x7/inc/hw_gpio.h:
+
+syscfg/ti_drivers_config.h:
---
+++ Day 2 source/empty_LP_CC1312R7_tirtos7_ticlang/Debug/subdir_vars.mk
@@ -0,0 +1,94 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../cc13x2x7_cc26x2x7_tirtos7.cmd 
+
+SYSCFG_SRCS += \
+../empty.syscfg 
+
+C_SRCS += \
+../empty.c \
+./syscfg/ti_devices_config.c \
+./syscfg/ti_drivers_config.c \
+./syscfg/ti_sysbios_config.c \
+../main_tirtos.c 
+
+GEN_FILES += \
+./syscfg/ti_devices_config.c \
+./syscfg/ti_drivers_config.c \
+./syscfg/ti_utils_build_compiler.opt \
+./syscfg/ti_sysbios_config.c 
+
+GEN_MISC_DIRS += \
+./syscfg 
+
+C_DEPS += \
+./empty.d \
+./syscfg/ti_devices_config.d \
+./syscfg/ti_drivers_config.d \
+./syscfg/ti_sysbios_config.d \
+./main_tirtos.d 
+
+GEN_OPTS += \
+./syscfg/ti_utils_build_compiler.opt 
+
+OBJS += \
+./empty.o \
+./syscfg/ti_devices_config.o \
+./syscfg/ti_drivers_config.o \
+./syscfg/ti_sysbios_config.o \
+./main_tirtos.o 
+
+GEN_MISC_FILES += \
+./syscfg/ti_drivers_config.h \
+./syscfg/ti_utils_build_linker.cmd.genlibs \
+./syscfg/ti_utils_build_linker.cmd.genmap \
+./syscfg/syscfg_c.rov.xs \
+./syscfg/ti_sysbios_config.h 
+
+GEN_MISC_DIRS__QUOTED += \
+"syscfg" 
+
+OBJS__QUOTED += \
+"empty.o" \
+"syscfg\ti_devices_config.o" \
+"syscfg\ti_drivers_config.o" \
+"syscfg\ti_sysbios_config.o" \
+"main_tirtos.o" 
+
+GEN_MISC_FILES__QUOTED += \
+"syscfg\ti_drivers_config.h" \
+"syscfg\ti_utils_build_linker.cmd.genlibs" \
+"syscfg\ti_utils_build_linker.cmd.genmap" \
+"syscfg\syscfg_c.rov.xs" \
+"syscfg\ti_sysbios_config.h" 
+
+C_DEPS__QUOTED += \
+"empty.d" \
+"syscfg\ti_devices_config.d" \
+"syscfg\ti_drivers_config.d" \
+"syscfg\ti_sysbios_config.d" \
+"main_tirtos.d" 
+
+GEN_FILES__QUOTED += \
+"syscfg\ti_devices_config.c" \
+"syscfg\ti_drivers_config.c" \
+"syscfg\ti_utils_build_compiler.opt" \
+"syscfg\ti_sysbios_config.c" 
+
+C_SRCS__QUOTED += \
+"../empty.c" \
+"./syscfg/ti_devices_config.c" \
+"./syscfg/ti_drivers_config.c" \
+"./syscfg/ti_sysbios_config.c" \
+"../main_tirtos.c" 
+
+SYSCFG_SRCS__QUOTED += \
+"../empty.syscfg" 
+
+
